1b30d8106fb67b7d22da25729720c9d49db2339d
Ncihanoglu <nihat_cihanoglu@hotmail.com>
2017-11-13 14:41:48 +0300

    Add files via upload

---
+++ src/main/java/com/userfront/Dao/PrimaryAccountDao.java
@@ -0,0 +1,9 @@
+package com.userfront.Dao;
+
+import com.userfront.domain.PrimaryAccount;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PrimaryAccountDao extends CrudRepository<PrimaryAccount,Long> {
+
+    PrimaryAccount findByAccountNumber (int accountNumber);
+}
